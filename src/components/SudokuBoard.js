1215aa4fcb031c33e22f3af7e2efd41d06721804
Will Hoback <will.hoback@gmail.com>
2020-02-11 14:27:44 -0500

    add board component

--- src/components/SudokuBoard.js
+++ src/components/SudokuBoard.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react"
 export default class SudokuBoard extends Component {
+
     render() {
-        return <div>{JSON.stringify(this.props.sudoku)}</div>
+        const { sudoku } = this.props;
+        return <div>
+            {sudoku.rows.map({ row } => <div className="row" key={row.index}>
+                {row.cols}
+            </div>}
+        </div>
     }
 }
